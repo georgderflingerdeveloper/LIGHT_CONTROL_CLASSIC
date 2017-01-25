fbe9c6b192f3fedcde74b7c545298a9e68492dc9
georgderflingerdeveloper <georgderflingerdeveloper@gmail.com>
2017-01-25 19:49:37 +0100

    add folder, move files into folders and a UDP sender for Kitchen light control, so far just for testing

--- light_visu_classic/light_visu_classic/Form_GroupControl.cs
+++ light_visu_classic/light_visu_classic/Form_GroupControl.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Windows.Forms;
 using Communication.UDP;
+using HomeAutomation.HardConfig;
 
 namespace light_visu_classic
 {
     public partial class Form_GroupControl : Form
     {
         Form_PowerMeter PowerMeter = new Form_PowerMeter();
+        UdpSend CommandoSender;
 
         public Form_GroupControl()
         {
             InitializeComponent();
+            CommandoSender = new UdpSend( "127.0.0.1", IPConfiguration.Port.PORT_LIGHT_CONTROL_COMMON );
         }
 
         private void bPowerMeter_Click( object sender, EventArgs e )
@@ -21,7 +24,7 @@ namespace light_visu_classic
 
         private void buttonLightGroupKitchenOn_Click(object sender, EventArgs e)
         {
-
+            CommandoSender?.SendString( ComandoString.TURN_ALL_LIGHTS_ON );
         }
     }
 }
